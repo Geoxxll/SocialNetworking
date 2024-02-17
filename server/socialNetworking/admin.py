7715cab173a365b7a9a5f20a8b74a7d03b2a13dd
Geoxxll <lxiao4@ualberta.ca>
2024-02-17 13:54:30 -0700

    follow model added and migrated

--- server/socialNetworking/admin.py
+++ server/socialNetworking/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-from .models import authors, followers
+from .models import authors, followers, follow
 # Register your models here.
---
+++ server/socialNetworking/models/follow.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Follow(models.Model):
+    '''
+        actor: there act as the follower
+        object_of_follow: people who recive the follow request
+    '''
+    actor = models.ForeignKey('Authors', on_delete=models.CASCADE, related_name='actor_follow_set')
+    object_of_follow = models.ForeignKey('Authors', on_delete=models.CASCADE, related_name='object_of_follow_set')
+
+    def __str__(self):
+        return f'{self.actor.displayName} want to follow {self.object_of_follow.displayName}'
