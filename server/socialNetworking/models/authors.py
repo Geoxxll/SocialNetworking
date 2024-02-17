bb6f0ca8e4e90030d6a10248e8a3a8501d891204
Geoxxll <lxiao4@ualberta.ca>
2024-02-16 19:13:13 -0700

    author model created and migrated

---
+++ server/socialNetworking/models/authors.py
@@ -0,0 +1,16 @@
+from django.db import models
+import uuid
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Author(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True)
+    url = models.URLField()
+    host = models.URLField()
+    displayName = models.CharField(max_length=100)
+    github = models.URLField()
+    profileImage = models.URLField()
+
+    def __str__(self):
+        return self.displayName
