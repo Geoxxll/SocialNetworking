e61f14f907d8c6b6679acb9f4c8b69b24eab0690
Geoxxll <lxiao4@ualberta.ca>
2024-02-21 13:01:19 -0700

    Posts model added

--- server/socialNetworking/admin.py
+++ server/socialNetworking/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models.authors import Author
 from .models.followers import Follower
 from .models.follow import Follow
+from .models.posts import Post
 
 # Register your models here.
 
 admin.site.register(Author)
 admin.site.register(Follower)
 admin.site.register(Follow)
+admin.site.register(Post)
---
+++ server/socialNetworking/models/posts.py
@@ -0,0 +1,32 @@
+from django.db import models
+import uuid
+
+class Post (models.Model):
+    contentTypesChoices = {
+        'COMMON_MARK':'text/markdown',
+        'PLAIN':'text/plain',
+        'BASE64':'application/base64',
+        'PNG':'image/png;base64',
+        'JPEG':'image/jpeg;base64',
+    }
+
+    visibilityChoices = models.TextChoices(
+        'PUBLIC',
+        'FRIENDS',
+        'UNLISTED',
+    )
+
+
+
+    title = models.CharField(max_length=100)
+    post_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    source = models.URLField()
+    origin = models.URLField()
+    description = models.CharField(max_length=300)
+    contentTpye = models.CharField(max_length=30, choices=contentTypesChoices, default='PLAIN')
+    content = models.BinaryField(null=True, blank=True)
+    visibility = models.CharField(max_length=10, choices=visibilityChoices, default='PUBLIC')
+    published_at =models.DateTimeField(auto_now_add=True)
+    author_of_posts = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='posts_set')
+
+
