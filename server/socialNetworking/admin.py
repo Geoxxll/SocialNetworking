168316d6a33939c2c5c535b670ad9ab007e87c89
Geoxxll <lxiao4@ualberta.ca>
2024-02-21 13:33:10 -0700

    comments model added and migrated

--- server/socialNetworking/admin.py
+++ server/socialNetworking/admin.py
@@ -3,6 +3,7 @@ from .models.authors import Author
 from .models.followers import Follower
 from .models.follow import Follow
 from .models.posts import Post
+from .models.comments import Comment
 
 # Register your models here.
 
@@ -10,3 +11,4 @@ admin.site.register(Author)
 admin.site.register(Follower)
 admin.site.register(Follow)
 admin.site.register(Post)
+admin.site.register(Comment)
