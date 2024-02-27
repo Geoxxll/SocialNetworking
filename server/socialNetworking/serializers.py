a9d5e2068b72f981fe7f5b75f5ac524c1a502f97
Travis Lee <travdlee@gmail.com>
2024-02-27 13:36:52 -0700

    Added comment serializer and adjusted author serializer

--- server/socialNetworking/serializers.py
+++ server/socialNetworking/serializers.py
@@ -1,14 +1,33 @@
 from rest_framework import serializers
 from .models.authors import Author
 from .models.followers import Follower
+from .models.comments import Comment
+from .models.likes import Like
+
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         # fields can be reduced later if no need.
-        fields = ['user', 'type', 'id', 'url', 'host', 'displayName', 'github', 'profileImage']
+        fields = ['type', 'id', 'url', 'host', 'displayName', 'github', 'profileImage']
 
 class FollowerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Follower
         # fields can be reduced later if no need.
         fields = ['type', 'follower', 'followee', 'followed_at']
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(source='comment_author')
+    published = serializers.DateTimeField(source='published_at')
+    id = serializers.UUIDField(source='comment_id')
+
+    class Meta:
+        model = Comment
+        fields = ['type', 'author', 'comment', 'contentType', 'published', 'id']
+
+# class LikeSerializer(serializers.ModelSerializer):
+#    author = AuthorSerializer(source='author_like')
+#
+#    class Meta:
+#        model = Like
+#        fields = ['context', 'summary', 'type', 'author', 'object']
