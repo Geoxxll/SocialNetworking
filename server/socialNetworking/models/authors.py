30d0017af118f39347e2b33c5a77fe05fbb8e296
Travis Lee <travdlee@gmail.com>
2024-03-14 16:11:23 -0600

    Added inbox fields to author model and date field to like model

--- server/socialNetworking/models/authors.py
+++ server/socialNetworking/models/authors.py
@@ -1,11 +1,16 @@
 from django.db import models
 import uuid
 from django.contrib.auth.models import User
+
+from .posts import Post
+from .comments import Comment
+from .likes import Like
+from .follow import Follow
 # Create your models here.
 
 class Author(models.Model):
     type = models.CharField(max_length=15, default='author', editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
     url = models.URLField()
     host = models.URLField()
@@ -14,6 +19,11 @@ class Author(models.Model):
     # TODO: url for default profileImage
     profileImage = models.URLField(null=True, blank=True)
     lastCommitFetch = models.DateTimeField(null=True, blank=True, editable=True)
+
+    postInbox = models.ManyToManyField(Post)
+    commentInbox = models.ManyToManyField(Comment)
+    likeInbox = models.ManyToManyField(Like)
+    followInbox = models.ManyToManyField(Follow)
 
     def __str__(self):
         return self.displayName + f": {self.lastCommitFetch}"
--- server/socialNetworking/models/likes.py
+++ server/socialNetworking/models/likes.py
@@ -4,6 +4,7 @@ from django.db import models
 class Like(models.Model):
     type = models.CharField(max_length=15, default='like', editable=False)
     summary = models.TextField(blank=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
 
     author_like = models.ForeignKey('Author', on_delete=models.CASCADE, related_name = "user_likes")
     like_post = models.ForeignKey('Post', on_delete=models.CASCADE, null=True, blank=False, related_name = "post_likes")
