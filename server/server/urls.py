adfbf97dca0fe02cc739f9eda0b71f3bec3d5577
Jawadchy2015 <jawad.chowdhury2018@gmail.com>
2024-03-26 06:35:33 -0600

    url added for api - swagger working

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
     path('', include('landing.urls'), name="index"),
     path('accounts/', include('allauth.urls')),
     path('', include('socialNetworking.urls')),
+    path("api/schema/", SpectacularAPIView.as_view(), name = "schema"),
+    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
