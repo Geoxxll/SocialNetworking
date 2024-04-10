c98cc37ea075d1004f08f7c7239438640c35a543
Jawadchy2015 <jawad.chowdhury2018@gmail.com>
2024-04-10 06:45:28 -0600

    Markdown preview partially implemented

--- server/socialNetworking/static/JS/post_list.js
+++ server/socialNetworking/static/JS/post_list.js
@@ -2,16 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
     var contentTypeSelect = document.getElementById('id_contentType');
     var contentInputWrapper = document.getElementById('content-input-wrapper');
     var contentTextArea = document.getElementById('id_text_content');
-
+    // for markedDown preview
+    var markedDownPreviewArea = document.getElementById('markdown-preview');
+    var markedDownPreviewTitle = document.getElementById('markdown-preview-title');
     // Function to toggle field visibility
     function toggleContentInput() {
         var contentType = contentTypeSelect.value;
         if (contentType === 'text/markdown' || contentType === 'text/plain') {
             contentTextArea.style.display = 'block' //show text area
         } else {
+            
             contentTextArea.style.display = 'none' // hide text area
             contentInputWrapper.innerHTML = '<input type="file" id="id_image_content" name="content">';
         }
+
+        
     }
 
     // Listen for changes to the contentType select box
